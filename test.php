1ca672891ba1b61c1580e5194f75a211ed92274f
reallyslick <reallyslick@gmail.com>
2025-06-10 20:25:03 -0600

    each row turned into a kv array. invalid rows filtered.

--- test.php
+++ test.php
@@ -2,20 +2,61 @@
 require __DIR__ . '/vendor/autoload.php';
 
 class fidelity{
+    
+                private $rowData = Array();
+                private $Headers = Array();
 	
+                //each row of the file is converted into an array
 		public function read_file_into_array()
 		{
                         print "Reading column headings into array";
-                        $rowData = Array();
+             
 			$row=1;
 			if (($handle = fopen("./documents/Portfolio_Positions_Jun-06-2025.csv", "r")) !== FALSE) {
 				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                                    array_push($rowData, $data);
+                                    if(empty($data[0])) // don't include blank data
+                                    {
+                                        break;
+                                    }
+                                
+                                    array_push($this->rowData, $data);
 				}
 				fclose($handle);
 			}
-			return $rowData;
+                        
+                        $this->setHeaders();
+                        return $this->rowData;
 		}
+                
+                
+                //the first row is the headers
+                public function setHeaders()
+                {
+                    /*Parse column headers into Array, first row of file*/
+                    $result = $this->rowData;
+                    $Headers = Array();
+                    foreach($result[0] as $header)
+                    {
+                        //print "Header: " . $header ."\n";
+                        array_push($this->Headers,$this->clean($header));
+                    }
+                    
+                }
+                
+                public function convertRowToKV($row)
+                {
+                    
+                    $kvArray= Array();
+                    foreach($this->Headers as $k=>$header)
+                    {
+                        
+                        $kvArray[$header] = empty($row[$k]) ? "" : $row[$k];
+                    }
+                    
+                    return $kvArray;
+                    
+                }
+                
                 
                 function clean($string) {
                     $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
@@ -31,29 +72,22 @@ class fidelity{
 $fi= new fidelity();
 
 
-/*Parse column headers into Array, first row of file*/
-$result = $fi->read_file_into_array();
-$Headers = Array();
-foreach($result[0] as $header)
+$data = $fi->read_file_into_array();
+
+
+foreach($data as $row)
 {
-    print "Header: " . $header ."\n";
-    array_push($Headers,$fi->clean($header));
-}
-
-
-//for each stock in the list
-$stockArray = Array();
-for ($x = 1; $x <= sizeOf($result); $x++) {
+    echo json_encode($row) . "\n";
     
-    //for each stocks parameter (~ Header)
-    foreach($result[$x] as $k=>$item)
+    $rowArray = $fi->convertRowToKV($row);
+    
+    foreach($rowArray as $k=>$v)
     {
-        print $Headers[$k] . "==> " . "" . $item ."";
-        
+        echo $k . "=" . $v . "\n" ;
     }
-  
-    print "\n";
+    echo "-----------------\n";
 }
+
 
 
 
