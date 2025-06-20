3f5faf8612ba078d7a808f861fdcab0cb3beeca4
reallyslick <reallyslick@gmail.com>
2025-06-19 20:39:23 -0600

    INSERT AND CHECK FILE EXISTS ENDPOINTS WORKING FOR FIDELITY APP

--- test.php
+++ test.php
@@ -33,7 +33,7 @@ class fidelity{
 		{
                     $directory = './documents/';
                     $files = array_diff(scandir($directory), array('..', '.'));
-                        print "Reading column headings into array";
+                        print "Reading column headings into array\n";
                         return $files;
 		}
                 
@@ -68,7 +68,7 @@ class fidelity{
                 
                 function sendPostRequest($data) {
                     
-                    $url = "https://cyrusbavarian.com/cynoteapi2/index.php/fidelity/test";
+                    $url = "https://cyrusbavarian.com/cynoteapi2/index.php/fidelity/FileInsert";
                             
                     // Initialize cURL session
                     $ch = curl_init($url);
@@ -94,6 +94,19 @@ class fidelity{
 
                     return $response;
                 }
+                
+                function sendGetRequest($params = []) {
+                    // Build the query string from the $params array
+                    $queryString = http_build_query($params);
+
+                    // Create the full URL
+                    $url = "https://cyrusbavarian.com/cynoteapi2/index.php/fidelity/fileexist" . '?' . $queryString;
+
+                    // Send the GET request and return the response
+                    $response = file_get_contents($url);
+                    return $response;
+                }
+
 
                
 
@@ -121,8 +134,49 @@ $files = $fi->getFiles();
 
 foreach($files as $fileName)
 {
+    //check to see if file was already inserted
+    $fileExists = $fi->sendGetRequest(Array("filename"=>$fileName));
+    $resultArray = json_decode($fileExists,true);
+    //echo $fileExists;
+    if($resultArray["fileExists"])
+    {
+        echo "file $fileName already inserted\n";
+        
+        $fname = "./documents/".$fileName;
+        //delete file after inserting...
+        if (file_exists($fname)) {
+            if (unlink($fname)) {
+                echo "File deleted successfully.";
+            } else {
+                echo "Error deleting the file.";
+            }
+        } else {
+            echo "File does not exist.";
+        }
+        
+        
+    }
+    
+    
+    //if not insert into database
     echo "Parsing file: " . $fileName . "\n";
     $data = $fi->read_file_into_array($fileName);
+    
+    // Use regex to extract the date parts (month, day, year)
+    preg_match('/([A-Za-z]+)-(\d{2})-(\d{4})/', $fileName, $matches);
+
+    if ($matches) {
+        // Convert month name to numeric format
+        $month = date('m', strtotime($matches[1])); 
+        $day = $matches[2];
+        $year = $matches[3];
+
+        // Format as YYYYMMDD
+        $formattedDate = "{$year}{$month}{$day}";
+
+        echo $formattedDate; // Output: 20250606
+    }
+
 
     //each stock is a row, several stocks in a file
     foreach($data as $i => $row)
@@ -130,29 +184,28 @@ foreach($files as $fileName)
         echo "Inserting Data: " . substr(implode(" ",$row), 0, 50) . "...";
         if($i>2)
         {
-            
+            //echo json_encode($row) . "\n";
 
-
-        //echo json_encode($row) . "\n";
-
-        $rowArray = $fi->convertRowToKV($row);
-
+            $rowArray = $fi->convertRowToKV($row);
+            $rowArray["FileName"] = $fileName;
+            $rowArray["dtts"] = $formattedDate;
             $response = $fi->sendPostRequest($rowArray);
-            //
-            
-            $ra = json_decode($response,true);
+
+            $ra = json_decode($response, true);
             //echo $ra["result"] . "";
-            if($ra["result"])
-            {
+            if ($ra["result"]) {
                 echo "Success!" . "\n";
-            }
-            else{
+            } else {
                 echo "fail!" . $response . "\n";
             }
-            
-                     sleep (2);
+
+            sleep(1);
         }
-}
+    }
+    
+    
+
+    
 }
 
 
